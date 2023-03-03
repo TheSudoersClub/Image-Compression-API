38a4a15ebf8a4a14d366293aea615e44a810e2b0
ayush <ayushahirrao@gmail.com>
2023-03-03 22:48:14 +0530

    added compress function

--- server/apis/compress.js
+++ server/apis/compress.js
@@ -4,19 +4,26 @@ const router = express.Router();
 // import the upload object from upload.js
 const { upload, getFileName } = require("../lib/multer/upload");
 
+// import the upload object from upload.js
+const compress = require("../lib/sharp/compress");
+
 // compress image route
-router.post('/', upload.single('file'), (req, res) => {
-
-    // todo - process the image 
-
+router.post('/', upload.single('file'), async (req, res) => {
     // updated unique name of file that has been send by client 
-    const fileName = getFileName();
+    const fileName = await getFileName();
+
+    // process the image 
+    let status = compress(fileName, 10);
 
     // send the image id from which one can download the image from download endpoint
-    console.log(fileName);
-    res.send({
-        id: fileName
-    })
+    if (status) {
+        res.send({
+            id: fileName
+        })
+    } else {
+        console.log("unable to compress the image");
+    }
+
 });
 
 // export the router
