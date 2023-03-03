f662e82cadb3d6e4c678be93716602a7bbbd13a6
ayush <ayushahirrao@gmail.com>
2023-03-03 22:04:09 +0530

    handled the uploaded image name (unique id)

--- server/apis/compress.js
+++ server/apis/compress.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-// upload data
-const upload = require("../lib/multer/upload");
+// import the upload object from upload.js
+const { upload, getFileName } = require("../lib/multer/upload");
 
 // compress image route
 router.post('/', upload.single('file'), (req, res) => {
 
     // todo - process the image 
 
-    // send updates image - req.file is the file that was uploaded
-    res.send(req.file.path);
+    // updated unique name of file that has been send by client 
+    const fileName = getFileName();
+
+    // send the image id from which one can download the image from download endpoint
+    console.log(fileName);
+    res.send({
+        id: fileName
+    })
 });
 
 // export the router
--- server/lib/multer/upload.js
+++ server/lib/multer/upload.js
@@ -1,17 +1,25 @@
 // multer to handle the uploads
 const multer = require("multer");
 
+// path module to handle file
+const path = require('path');
+
+let fileName = null;
+
 // Set up Multer for file uploads
 const storage = multer.diskStorage({
     // Set the destination directory for uploaded files
     destination: function (req, file, cb) {
         // since the api is running from server.js so file path will be uploads instead of ../uploads
-        cb(null, 'uploads');
+        cb(null, 'server/uploads/images');
     },
 
     // Set the filename for uploaded files
     filename: function (req, file, cb) {
-        cb(null, file.originalname + Date.now);
+        // generate the unique file name
+        fileName = (path.parse(file.originalname).name) + '-' + Date.now() + path.extname(file.originalname)
+        cb(null, fileName);
+
     },
 });
 
@@ -20,4 +28,8 @@ const upload = multer({
     storage,
 });
 
-module.exports = upload
+// export upload
+module.exports = {
+    upload,
+    getFileName: () => fileName
+};
